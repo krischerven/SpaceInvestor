f5f046ebda7f38990a8ecc1c633608db0825de28
Kris Cherven <krischerven@gmail.com>
2019-09-16 16:20:43 -0400

    Update Hasher.java

--- src/main/java/co/gc/space/Hasher.java
+++ src/main/java/co/gc/space/Hasher.java
@@ -3,30 +3,26 @@ import java.security.spec.KeySpec;
 import java.util.Base64;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
-//import java.security.SecureRandom;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
 public final class Hasher {
 	public static String Hash(String in) {
-        byte[] salt = new byte[16];
-        //(new SecureRandom()).nextBytes(salt);
-        KeySpec spec = new PBEKeySpec(in.toCharArray(), salt, 65536, 128);
-        SecretKeyFactory f = null;
+        final KeySpec spec = new PBEKeySpec(in.toCharArray(), new byte[16], 65536, 128);
+        SecretKeyFactory skf = null;
 		try {
-			f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
         byte[] hash = null;
 		try {
-			hash = f.generateSecret(spec).getEncoded();
+			hash = skf.generateSecret(spec).getEncoded();
 		} catch (InvalidKeySpecException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        Base64.Encoder enc = Base64.getEncoder();
-        return enc.encodeToString(hash);
+        return Base64.getEncoder().encodeToString(hash);
 	}
 }
